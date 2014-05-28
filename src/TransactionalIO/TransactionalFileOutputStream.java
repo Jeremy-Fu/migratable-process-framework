10faaad6ee35198b05c6c43ef11be1602ab29fc7
Jeremy Fu <gengf@andrew.cmu.edu>
2014-05-27 22:33:26 -0400

    Implement transacation output

--- src/TransactionalIO/TransactionalFileOutputStream.java
+++ src/TransactionalIO/TransactionalFileOutputStream.java
@@ -1,5 +1,27 @@
 package TransactionalIO;
 
-public class TransactionalFileOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.RandomAccessFile;
+
+public class TransactionalFileOutputStream extends OutputStream {
+	private String filename;
+	private long counter;
+	private String permission;
+	
+	public TransactionalFileOutputStream (String filename, String permission) {
+		this.counter = 0L;
+		this.filename = filename;
+		this.permission = permission;
+	}
+	
+	@Override
+	public void write(int aByte) throws IOException {
+		RandomAccessFile file = new RandomAccessFile(this.filename, this.permission);
+		file.seek(this.counter);
+		file.write(aByte);
+		counter++;
+		file.close();
+	}
 
 }
---
+++ src/test/TransactionalFileOutputStreamTest.java
@@ -0,0 +1,19 @@
+package test;
+
+import java.io.IOException;
+
+import TransactionalIO.TransactionalFileOutputStream;
+
+public class TransactionalFileOutputStreamTest {
+	public static void main (String[] args) {
+		TransactionalFileOutputStream file = new TransactionalFileOutputStream("output_test.txt","rw");
+		byte[] output = {65, 66, 67, 68, 69, 10, 70, 71, 72};
+		try {
+			for (int i = 0; i < output.length; i++) {
+				file.write(output[i]);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
